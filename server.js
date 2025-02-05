5f8c66b754a7dd2647ecb33ceff23af75b8862e7
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2025-02-05 13:44:30 +0100

    impostazioni

--- server.js
+++ server.js
@@ -9,6 +9,60 @@ app.use(bodyParser.urlencoded({
 const path = require('path');
 app.use("/", express.static(path.join(__dirname, "public")));
 
+const fs = require('fs');
+const mysql = require('mysql2');
+const conf = JSON.parse(fs.readFileSync('conf.json'));
+const connection = mysql.createConnection(conf);
+
+
+const executeQuery = (sql) => {
+    return new Promise((resolve, reject) => {      
+          connection.query(sql, function (err, result) {
+             if (err) {
+                console.error(err);
+                reject();     
+             }   
+             console.log('done');
+             resolve(result);         
+       });
+    })
+ }
+
+ const createTable = () => {
+    return executeQuery(`
+    CREATE TABLE IF NOT EXISTS todo
+       ( id INT PRIMARY KEY AUTO_INCREMENT, 
+          name VARCHAR(255) NOT NULL, 
+          completed BOOLEAN ) 
+       `);      
+ }
+
+ const insert = (todo) => {
+    const template = `
+    INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
+       `;
+    let sql = template.replace("$NAME", todo.name);
+    sql = sql.replace("$COMPLETED", todo.completed);
+    return executeQuery(sql); 
+ }
+
+ const select = () => {
+    const sql = `
+    SELECT id, name, completed FROM todo 
+       `;
+    return executeQuery(sql); 
+ }
+
+
+ createTable().then(() => {
+    insert({name: "test " + new Date().getTime(), completed: false}).then((result) => {
+       select().then(console.log);
+    });
+ });
+
+
+
+
 let todos = [];
 
 app.post("/todo/add", (req, res) => {
